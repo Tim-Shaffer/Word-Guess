b5915b709a860436cae65b66183df6f33ee1c411
Tim-Shaffer <tcshaf24@ptd.net>
2020-01-08 13:55:13 -0500

    moved check letter to a function so that the last guess would alert when wrong correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -52,48 +52,86 @@ function correctGuess(hidden, guess, letter) {
 	}
 
 	return hidden;
+};
+
+// Make the main processing into a function so it can be called for any guess situation!
+function checkGuess(letter) {
+		
+	// check to see if the letter was already guessed or already found
+	if (guessLetters.indexOf(letter) === -1 && hiddenWord.indexOf(letter) ){
+
+		// Search the guess word to see if character is found
+		if (answer.indexOf(letter) != -1){
+
+			// see if the letter matches any positions in the answer and change those positions 
+			correctGuess(hiddenWord, answer, letter);
+
+			// update the page with the new results
+			document.querySelector('#hiddenword').innerHTML = dispArray(hiddenWord);
+
+		}
+		else {
+
+			// Only populate a guess array with the values input when the letter is not found
+			guessLetters.push(letter); 
+
+			// update the display with the incorrectly guessed letters
+			document.querySelector('#guesses').innerHTML = dispArray(guessLetters);
+
+			// Calculate the number of remaining guesses allowed as the difference between the max and the wrong guesses
+			remainingGuesses = allowedGuesses - guessLetters.length;
+	
+		}
+	
+	};
+
+	return remainingGuesses;
 }
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
 
+	// Determines which key was pressed and converts to lowercase for ease of comparison
+	var userGuess = event.key.toLowerCase();
+
 	// Only perform this processing while there are guesses allowed!
-	if (remainingGuesses > 0) {
-		// Determines which key was pressed and converts to lowercase for ease of comparison
-		var userGuess = event.key.toLowerCase();  
+	if (remainingGuesses > 1) {	
+		// // check to see if the letter was already guessed or already found
+		// if (guessLetters.indexOf(userGuess) === -1 && hiddenWord.indexOf(userGuess) ){
+
+		// 	// Search the guess word to see if character is found
+		// 	if (answer.indexOf(userGuess) != -1){
+
+		// 		// see if the letter matches any positions in the answer and change those positions 
+		// 		correctGuess(hiddenWord, answer, userGuess);
+
+		// 		// update the page with the new results
+		// 		document.querySelector('#hiddenword').innerHTML = dispArray(hiddenWord);
+
+		// 	}
+		// 	else {
+
+		// 		// Only populate a guess array with the values input when the letter is not found
+		// 		guessLetters.push(userGuess); 
+
+		// 		// update the display with the incorrectly guessed letters
+		// 		document.querySelector('#guesses').innerHTML = dispArray(guessLetters);
+
+		// 		// Calculate the number of remaining guesses allowed as the difference between the max and the wrong guesses
+		// 		remainingGuesses = allowedGuesses - guessLetters.length;
 		
-		// check to see if the letter was already guessed or already found
-		if (guessLetters.indexOf(userGuess) === -1 && hiddenWord.indexOf(userGuess) ){
-
-			// Search the guess word to see if character is found
-			if (answer.indexOf(userGuess) != -1){
-
-				// see if the letter matches any positions in the answer and change those positions 
-				correctGuess(hiddenWord, answer, userGuess);
-
-				// update the page with the new results
-				document.querySelector('#hiddenword').innerHTML = dispArray(hiddenWord);
-
-			}
-			else {
-
-				// Only populate a guess array with the values input when the letter is not found
-				guessLetters.push(userGuess); 
-
-				// update the display with the incorrectly guessed letters
-				document.querySelector('#guesses').innerHTML = dispArray(guessLetters);
-
-				// Calculate the number of remaining guesses allowed as the difference between the max and the wrong guesses
-				remainingGuesses = allowedGuesses - guessLetters.length;
+		// 	}
 		
-			}
-		
-		};
+		// };
+		checkGuess(userGuess);
 	}
-	// Too many guesses so this is a loss
+	// Last Guess!
 	else {
-		alert("Sorry, you lose!");
+		if (checkGuess(userGuess) === 0) {
+			alert("Sorry, you lose!");
+		}
 	}
 
 };
+
 
