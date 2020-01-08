23b72795a5346191bd14ced05804ca71248b1a35
Tim-Shaffer <tcshaf24@ptd.net>
2020-01-08 14:13:17 -0500

    added Remaining Guess logging to the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -85,6 +85,9 @@ function checkGuess(letter) {
 	
 	};
 
+	// update the display with the Number of Remaining Guesses
+	document.querySelector('#guessesRemaining').innerHTML = "<p>Guesses Remaining: " +  remainingGuesses + "</p>";
+
 	return remainingGuesses;
 }
 
@@ -96,36 +99,11 @@ document.onkeyup = function(event) {
 
 	// Only perform this processing while there are guesses allowed!
 	if (remainingGuesses > 1) {	
-		// // check to see if the letter was already guessed or already found
-		// if (guessLetters.indexOf(userGuess) === -1 && hiddenWord.indexOf(userGuess) ){
 
-		// 	// Search the guess word to see if character is found
-		// 	if (answer.indexOf(userGuess) != -1){
-
-		// 		// see if the letter matches any positions in the answer and change those positions 
-		// 		correctGuess(hiddenWord, answer, userGuess);
-
-		// 		// update the page with the new results
-		// 		document.querySelector('#hiddenword').innerHTML = dispArray(hiddenWord);
-
-		// 	}
-		// 	else {
-
-		// 		// Only populate a guess array with the values input when the letter is not found
-		// 		guessLetters.push(userGuess); 
-
-		// 		// update the display with the incorrectly guessed letters
-		// 		document.querySelector('#guesses').innerHTML = dispArray(guessLetters);
-
-		// 		// Calculate the number of remaining guesses allowed as the difference between the max and the wrong guesses
-		// 		remainingGuesses = allowedGuesses - guessLetters.length;
-		
-		// 	}
-		
-		// };
 		checkGuess(userGuess);
+
 	}
-	// Last Guess!
+	// Last Guess! Calls the alert on the loss if the last guess is WRONG!
 	else {
 		if (checkGuess(userGuess) === 0) {
 			alert("Sorry, you lose!");
@@ -133,5 +111,6 @@ document.onkeyup = function(event) {
 	}
 
 };
+
 
 
